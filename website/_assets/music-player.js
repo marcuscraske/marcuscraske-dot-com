4874d78533680c1ec4c459fa4829eef899299490
limpygnome <limpygnome@gmail.com>
2016-10-27 07:44:52 +0100

    Still need to finish player, having issues with CloudWatch alarms for CloudFront.

--- website/_assets/music-player.js
+++ website/_assets/music-player.js
@@ -15,14 +15,14 @@ limpygnome.audio.player = (function(){
             '</audio>' +
             '<div class="player">' +
                 '<div class="controls">' +
-                    '<img alt="Play/Pause" src="/assets/music/player/play.png" id="play" onclick="limpygnome.audio.player.audioPlayerPlayToggle(this);" />' +
-                    '<img alt="Restart" src="/assets/music/player/restart.png" id="restart" onclick="limpygnome.audio.player.audioPlayerRestart(this);" />' +
-                    '<img alt="Next" src="/assets/music/player/next.png" id="next" onclick="limpygnome.audio.player.audioPlayerNext(this);" />' +
+                    '<img alt="Play/Pause" src="/assets/music/player/play.png" id="play" onclick="limpygnome.audio.player.playPause(this);" />' +
+                    '<img alt="Restart" src="/assets/music/player/restart.png" id="restart" onclick="limpygnome.audio.player.restart(this);" />' +
+                    '<img alt="Next" src="/assets/music/player/next.png" id="next" onclick="limpygnome.audio.player.next(this);" />' +
                 '</div>' +
-                '<input type="range" class="seeker" min="0" max="100" value="0" id="seeker" onchange="limpygnome.audio.player.audioPlayerSeek(this);" />' +
+                '<input type="range" class="seeker" min="0" max="100" value="0" id="seeker" onchange="limpygnome.audio.player.seek(this);" />' +
                 '<div class="volume">' +
-                    '<img alt="Volume" src="/assets/music/player/unmuted.png" id="volume" onclick="limpygnome.audio.player.audioPlayerMuteToggle(this);" />' +
-                    '<input type="range" min="0" max="100" value="100" id="volume_seeker" onchange="limpygnome.audio.player.audioPlayerVolume(this);" />' +
+                    '<img alt="Volume" src="/assets/music/player/unmuted.png" id="volume" onclick="limpygnome.audio.player.muteToggle(this);" />' +
+                    '<input type="range" min="0" max="100" value="100" id="volume_seeker" onchange="limpygnome.audio.player.volume(this);" />' +
                 '</div>' +
             '</div>' +
             '<ul>' +
@@ -33,7 +33,7 @@ limpygnome.audio.player = (function(){
 
     var htmlItem = [
         '<li>' +
-            '<a href="%URL%" onclick="return limpygnome.audio.player.audioPlayerChange(this);">' +
+            '<a href="%URL%" onclick="return limpygnome.audio.player.change(this);">' +
                 '<img src="%THUMBNAIL%" alt="Album thumbnail" />' +
                 '<span>%TITLE%</span>' +
             '</a>' +
@@ -65,244 +65,249 @@ limpygnome.audio.player = (function(){
 
         // Write HTML to page
         document.write(player);
+
+        // Setup hooks for any uninitialized players i.e. one above
+        setupHooksAndFirstSong();
     };
 
 
-//    function audioPlayerHookAll()
-//    {
-//        var nodes = document.getElementsByTagName("DIV");
-//
-//        var node;
-//        for(i = 0; i < nodes.length; i++)
-//        {
-//            node = nodes[i];
-//            if(node.className != null && node.className == "music_player")
-//            {
-//                // Find first element, hook as current song
-//                var elementsPlaylistItems = node.getElementsByTagName("A");
-//                var elementsAudio = node.getElementsByTagName("AUDIO");
-//
-//                if(elementsPlaylistItems.length > 0 && elementsAudio.length > 0)
-//                {
-//                    var player = elementsAudio[0];
-//                    var item = elementsPlaylistItems[0];
-//                    player.currentSong = item;
-//
-//                    // Hook for auto-play of playlist
-//                    player.onended = function(e)
-//                    {
-//                        audioPlayerNext(e.target);
-//                    };
-//
-//                    // Hook for seeker update
-//                    player.ontimeupdate = function(e)
-//                    {
-//                        var player = e.target;
-//                        var progress = (player.currentTime / player.duration) * 100.0;
-//                        var seeker = audioPlayerGetControl(player, "INPUT", "seeker");
-//                        seeker.value = progress;
-//                    };
-//
-//                    // Hook for mute / volume update
-//                    player.onvolumechange = function(e)
-//                    {
-//                        var player = e.target;
-//                        var button = audioPlayerGetControl(player, "IMG", "volume");
-//                        button.src = "/content/music/player/" + (player.muted ? "muted" : "unmuted") + ".png";
-//                        var volumeSeeker = audioPlayerGetControl(player, "INPUT", "volume_seeker");
-//                        volumeSeeker.value = player.volume * 100.0;
-//                    };
-//
-//                    // Hook for play/pause update
-//                    player.onplay = player.onpause = function(e)
-//                    {
-//                        var player = e.target;
-//                        var button = audioPlayerGetControl(player, "IMG", "play");
-//                        button.src = "/content/music/player/" + (player.paused ? "play" : "pause") + ".png";
-//                    };
-//                }
-//            }
-//        }
-//
-//    }
-//
-//    function audioPlayerGetControl(player, type, id)
-//    {
-//        var controls = player.parentNode.getElementsByTagName(type);
-//        var control;
-//        for(i = 0; i < controls.length; i++)
-//        {
-//            control = controls[i];
-//            if(control.id != null && control.id == id)
-//            {
-//                return control;
-//            }
-//        }
-//    }
-//
-//    function audioPlayerGet(child)
-//    {
-//        var parent = child.parentNode;
-//        var nodes;
-//        var node;
-//        var tag;
-//
-//        for(var level = 0; level < 5; level++)
-//        {
-//            nodes = parent.childNodes;
-//
-//            for(i = 0; i < nodes.length; i++)
-//            {
-//                node = nodes[i];
-//                tag = node.tagName;
-//
-//                if(tag != null && tag.toLowerCase() == 'audio')
-//                {
-//                    return node;
-//                }
-//            }
-//
-//            parent = parent.parentNode;
-//        }
-//
-//        return null;
-//    }
-//
-//    function audioPlayerSeek(element)
-//    {
-//        var value = element.value;
-//        var player = audioPlayerGet(element);
-//        player.currentTime = player.duration * (value/100.0);
-//    }
-//
-//    function audioPlayerPlayToggle(element)
-//    {
-//        var player = audioPlayerGet(element);
-//        if(player.paused)
-//        {
-//            player.play();
-//        }
-//        else
-//        {
-//            player.pause();
-//        }
-//    }
-//
-//    function audioPlayerVolume(element)
-//    {
-//        var player = audioPlayerGet(element);
-//        player.volume = element.value/100.0;
-//    }
-//
-//    function audioPlayerMuteToggle(element)
-//    {
-//        var player = audioPlayerGet(element);
-//        player.muted = !player.muted;
-//    }
-//
-//    function audioPlayerRestart(element)
-//    {
-//        var player = audioPlayerGet(element);
-//        player.pause();
-//        player.load();
-//        player.play();
-//    }
-//
-//    function audioPlayerNext(element)
-//    {
-//        // Find player from element
-//        var player;
-//        if(element.tagName == null)
-//        {
-//            alert("Cannot play next song, player error.");
-//            return;
-//        }
-//        else if(element.tagName.toLowerCase() == "audio")
-//        {
-//            player = element;
-//        }
-//        else if((player = audioPlayerGet(element)) == null)
-//        {
-//            alert("Cannot play next song, player not found.");
-//            return;
-//        }
-//
-//        var currentElement = player.currentSong;
-//
-//        if(currentElement == null)
-//        {
-//            alert("Cannot play next song, player error.");
-//            return;
-//        }
-//
-//        // Find index of list, pick next or first index
-//        var ceParentNodes = currentElement.parentNode.parentNode.childNodes;
-//        var index = -1;
-//        var node;
-//        var nextNode;
-//        for(i = 0; i < ceParentNodes.length; i++)
-//        {
-//            node = ceParentNodes[i].firstElementChild;
-//            if(node != null && node.href != null)
-//            {
-//                // Select first node, incase no next node is found...
-//                if(nextNode == null)
-//                {
-//                    nextNode = node;
-//                }
-//
-//                if(index != -1)
-//                {
-//                    // We've found the next href element after the current one
-//                    nextNode = node;
-//                    break;
-//                }
-//                else
-//                {
-//                    // Check if first index
-//                    if(node.href == currentElement.href)
-//                    {
-//                        index = i;
-//                    }
-//                }
-//            }
-//        }
-//
-//        // Check we found an index
-//        if(nextNode == null)
-//        {
-//            alert("Failed to play next item, playlist error.");
-//            return;
-//        }
-//
-//        audioPlayerChange(nextNode);
-//    }
-//
-//    function audioPlayerChange(element)
-//    {
-//        var url = element.href;
-//        var player = audioPlayerGet(element);
-//
-//        if(player != null)
-//        {
-//            // Play clicked song
-//            player.pause();
-//            player.src = url;
-//            player.load();
-//            player.play();
-//
-//            // Remove class of old element
-//            if(player.currentSong != null)
-//            {
-//                player.currentSong.parentNode.className = null;
-//            }
-//
-//            // Save element to player
-//            player.currentSong = element;
-//            player.currentSong.parentNode.className = "playing";
-//        }
-//
-//    }
+    var setupHooksAndFirstSong = function()
+    {
+        var nodes = document.getElementsByTagName("DIV");
+
+        var node;
+        for(i = 0; i < nodes.length; i++)
+        {
+            node = nodes[i];
+            if(node.className != null && node.className == "music-player" && node.isSetup == null)
+            {
+                // Find first element, hook as current song
+                var elementsPlaylistItems = node.getElementsByTagName("A");
+                var elementsAudio = node.getElementsByTagName("AUDIO");
+
+                if(elementsPlaylistItems.length > 0 && elementsAudio.length > 0)
+                {
+                    var player = elementsAudio[0];
+                    var item = elementsPlaylistItems[0];
+                    player.currentSong = item;
+
+                    // Hook for auto-play of playlist
+                    player.onended = function(e)
+                    {
+                        audioPlayerNext(e.target);
+                    };
+
+                    // Hook for seeker update
+                    player.ontimeupdate = function(e)
+                    {
+                        var player = e.target;
+                        var progress = (player.currentTime / player.duration) * 100.0;
+                        var seeker = getPlayerControl(player, "INPUT", "seeker");
+                        seeker.value = progress;
+                    };
+
+                    // Hook for mute / volume update
+                    player.onvolumechange = function(e)
+                    {
+                        var player = e.target;
+                        var button = getPlayerControl(player, "IMG", "volume");
+                        button.src = "/content/music/player/" + (player.muted ? "muted" : "unmuted") + ".png";
+                        var volumeSeeker = getPlayerControl(player, "INPUT", "volume_seeker");
+                        volumeSeeker.value = player.volume * 100.0;
+                    };
+
+                    // Hook for play/pause update
+                    player.onplay = player.onpause = function(e)
+                    {
+                        var player = e.target;
+                        var button = getPlayerControl(player, "IMG", "play");
+                        button.src = "/content/music/player/" + (player.paused ? "play" : "pause") + ".png";
+                    };
+                }
+
+                // Set flag to indicate player has been setup
+                node.isSetup = true;
+            }
+        }
+    };
+
+    var getPlayerControl = function(player, type, id)
+    {
+        var controls = player.parentNode.getElementsByTagName(type);
+        var control;
+        for(i = 0; i < controls.length; i++)
+        {
+            control = controls[i];
+            if(control.id != null && control.id == id)
+            {
+                return control;
+            }
+        }
+    };
+
+    var getPlayer = function(child)
+    {
+        var parent = child.parentNode;
+        var nodes;
+        var node;
+        var tag;
+
+        for(var level = 0; level < 5; level++)
+        {
+            nodes = parent.childNodes;
+
+            for(i = 0; i < nodes.length; i++)
+            {
+                node = nodes[i];
+                tag = node.tagName;
+
+                if(tag != null && tag.toLowerCase() == 'audio')
+                {
+                    return node;
+                }
+            }
+
+            parent = parent.parentNode;
+        }
+
+        return null;
+    };
+
+    var seek = function(element)
+    {
+        var value = element.value;
+        var player = getPlayer(element);
+        player.currentTime = player.duration * (value/100.0);
+    };
+
+    var playPause = function(element)
+    {
+        var player = getPlayer(element);
+        if(player.paused)
+        {
+            player.play();
+        }
+        else
+        {
+            player.pause();
+        }
+    };
+
+    var volume = function(element)
+    {
+        var player = getPlayer(element);
+        player.volume = element.value/100.0;
+    };
+
+    var muteToggle = function(element)
+    {
+        var player = getPlayer(element);
+        player.muted = !player.muted;
+    };
+
+    var restart = function(element)
+    {
+        var player = getPlayer(element);
+        player.pause();
+        player.load();
+        player.play();
+    };
+
+    var next = function(element)
+    {
+        // Find player from element
+        var player;
+        if(element.tagName == null)
+        {
+            alert("Cannot play next song, player error.");
+            return;
+        }
+        else if(element.tagName.toLowerCase() == "audio")
+        {
+            player = element;
+        }
+        else if((player = getPlayer(element)) == null)
+        {
+            alert("Cannot play next song, player not found.");
+            return;
+        }
+
+        var currentElement = player.currentSong;
+
+        if(currentElement == null)
+        {
+            alert("Cannot play next song, player error.");
+            return;
+        }
+
+        // Find index of list, pick next or first index
+        var ceParentNodes = currentElement.parentNode.parentNode.childNodes;
+        var index = -1;
+        var node;
+        var nextNode;
+        for(i = 0; i < ceParentNodes.length; i++)
+        {
+            node = ceParentNodes[i].firstElementChild;
+            if(node != null && node.href != null)
+            {
+                // Select first node, incase no next node is found...
+                if(nextNode == null)
+                {
+                    nextNode = node;
+                }
+
+                if(index != -1)
+                {
+                    // We've found the next href element after the current one
+                    nextNode = node;
+                    break;
+                }
+                else
+                {
+                    // Check if first index
+                    if(node.href == currentElement.href)
+                    {
+                        index = i;
+                    }
+                }
+            }
+        }
+
+        // Check we found an index
+        if(nextNode == null)
+        {
+            alert("Failed to play next item, playlist error.");
+            return;
+        }
+
+        audioPlayerChange(nextNode);
+    };
+
+    var change = function(element)
+    {
+        var url = element.href;
+        var player = getPlayer(element);
+
+        if(player != null)
+        {
+            // Play clicked song
+            player.pause();
+            player.src = url;
+            player.load();
+            player.play();
+
+            // Remove class of old element
+            if(player.currentSong != null)
+            {
+                player.currentSong.parentNode.className = null;
+            }
+
+            // Save element to player
+            player.currentSong = element;
+            player.currentSong.parentNode.className = "playing";
+        }
+
+    };
 
     return {
         setup : setup
