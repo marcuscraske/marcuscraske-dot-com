21cc9ae640e72e941c05dbe94c29622d4ae5ada2
limpygnome <limpygnome@gmail.com>
2016-10-28 07:42:32 +0100

    Progress with Ansible.

--- aws/README.md
+++ aws/README.md
@@ -1,7 +1,27 @@
 # Amazon Web Services (AWS) Deployment
 This directory is used for AWS deployments.
 
-## Resources
-Useful resource, much credit owed to the following:
+## Credentials
+You will need to put the following vars:
+````
+AWS_ACCESS_KEY_ID
+AWS_ACCESS_KEY
+````
+
+In a vault file:
+````
+(cd ansible/roles/setup/vars && ansible-vault create key.yml)
+````
+
+This is ignored by Git.
+
+## Dependencies
+````
+sudo apt-get install ansible python-pip
+sudo pip install boto
+````
+
+## Useful Resources
+Much credit owed to the following for CloudFormation:
 
 <https://raw.githubusercontent.com/mikery/aws-system-administration/master/cloudformation_stacks/s3_static_website_with_cloudfront_and_route_53/s3_static_website_with_cloudfront_and_route_53.template>
--- aws/deploy.sh
+++ aws/deploy.sh
@@ -13,26 +13,28 @@ EOF
 
 echo -e "\033[1;32m${SCRIPT_HEADER}\033[1;32m"
 
+# Reset terminal colour
+echo -e "\033[0m"
+
 # Define paths
-PATH_CURR=$(pwd)
-PATH_BASE=$(dirname "${PATH_CURR}")
+PATH_BASE=$(pwd)
 
 # Determine inventory file to use
 INVENTORY="hosts_inventory"
 
 # Build extra vars
 # -- Path to base of all files
-EXTRA_VARS+="root_path=\"${PATH_BASE}\" "
+EXTRA_VARS+="aws_root=\"${PATH_BASE}\" "
 
 # Attach any args after first as variables
 EXTRA_VARS+="${@:2} "
 
 # Build tags
-if [[ -z "${1}" || "${1}" == "all" ]]; then
+if [[ -z "${1}" ]]; then
 
     if [[ -z "${1}" ]]; then
         echo "Available tags:"
-        echo "$(ls roles)"
+        echo "$(ls ansible/roles)"
         echo ""
         echo "...or use tag 'all'."
         echo ""
@@ -47,9 +49,6 @@ else
     DEPLOY_TAGS="${1}"
 fi
 
-# Reset terminal colour
-echo -e "\033[0m"
-
 # Check user wants to continue
 echo "You are about to perform an automated deployment with the following tags:"
 echo -e "\033[1;31m${DEPLOY_TAGS}\033[0m"
@@ -61,4 +60,6 @@ echo "Running deployment..."
 
 
 # Run deployment
-ansible-playbook deploy.yml -vvvv -i "${INVENTORY}" --extra-vars "${EXTRA_VARS}" --tags "${DEPLOY_TAGS}"
+ansible-playbook    ansible/deploy.yml -vvvv -i "ansible/${INVENTORY}" \
+                    --ask-vault-pass \
+                    --extra-vars "${EXTRA_VARS}" --tags "${DEPLOY_TAGS}"
