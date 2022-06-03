75856a3efec9c8636457980fb96d50274993a893
limpygnome <limpygnome@gmail.com>
2022-06-03 17:48:40 +0100

    Adding DNS for home.

--- deployment/dns/marcus-dot-engineer.tf
+++ deployment/dns/marcus-dot-engineer.tf
@@ -7,3 +7,12 @@ module "marcus-dot-engineer-redirect" {
     cloudflare_zone_id = cloudflare_zone.marcus-dot-engineer.id
     cloudflare_hostname = cloudflare_zone.marcus-dot-engineer.zone
 }
+
+resource "cloudflare_record" "home" {
+    zone_id     = cloudflare_zone.marcus-dot-engineer.id
+    name        = "home"
+    value       = "88.98.227.3"
+    type        = "A"
+    proxied     = false
+    ttl         = 1
+}
